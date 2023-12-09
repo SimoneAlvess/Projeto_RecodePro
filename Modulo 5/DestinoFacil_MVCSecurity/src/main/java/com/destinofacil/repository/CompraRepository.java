0f6f6337d4034e09ef3aa16a85236ec813517b1f
SimoneAlvess <simonealvess274@gmail.com>
2023-12-09 13:17:48 -0300

    update

--- Modulo 5/DestinoFacil_MVCSecurity/src/main/java/com/destinofacil/repository/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.destinofacil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.destinofacil.entity.Compra;
-
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-
-}
--- Modulo 5/DestinoFacil_MVCSecurity/src/main/java/com/destinofacil/repository/PromocaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.destinofacil.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.destinofacil.entity.Promocao;
-
-public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
-
-}
